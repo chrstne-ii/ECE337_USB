a1ac03adef6de4a41b46153caad00f198737b7f2
ricefieldrat <150633427+ricefieldrat@users.noreply.github.com>
2026-04-11 20:44:53 -0400

    sub_testbench

--- source/ahb_subordinate.sv
+++ source/ahb_subordinate.sv
@@ -1,9 +1,10 @@
 `timescale 1ns / 10ps
 
 module ahb_subordinate #(
-    // parameters
 ) (
-    input logic clk, n_rst
+    input logic clk, n_rst,
+    input logic hsel, hwrite, [1:0] hsize, [3:0] haddr, [31:0] hwdata, [2:0] hburst
+    output logic d_mode, [31:0] hrdata, hresp, hready
 );
 
 
--- testbench/tb_ahb_subordinate.sv
+++ testbench/tb_ahb_subordinate.sv
@@ -11,6 +11,11 @@ module tb_ahb_subordinate ();
     end
 
     logic clk, n_rst;
+    logic hsel, hwrite, d_mode, hresp, hready;
+    logic [1:0] hsize, htrans;
+    logic [3:0] haddr;
+    logic [2:0] hburst;
+    logic [31:0] hwdata, hrdata;
 
     // clockgen
     always begin
@@ -32,12 +37,80 @@ module tb_ahb_subordinate ();
     end
     endtask
 
+    task write;
+    input logic [3:0] addr;
+    input logic [31:0] data;
+    input logic [1:0] size;
+    input logic [2:0] burst;
+    input logic [1:0] mode;
+    begin
+        @(negedge clk);
+        hsel = 1;
+        hwrite = 1;
+        hsize = size;
+        haddr = addr;
+        hwdata = data;
+        hburst = burst;
+
+        @(posedge clk);
+        if(hsize == 2'b00) begin
+            @(posedge clk);
+            if(hready) begin
+                hsel = 0;
+            end
+            
+        end
+        else if(hsize == 2'b01) begin
+            
+        end
+        else if(hsize == 2'b10) begin
+            
+        end
+        else begin
+
+        end
+    end
+    endtask
+
+    task read(addr);
+    input logic [3:0] addr;
+    input logic [31:0] data;
+    input logic [1:0] size;
+    input logic [2:0] burst;
+    input logic [1:0] mode;
+    begin
+        @(negedge clk);
+        hsel = 1;
+        hwrite = 1;
+        hsize = size;
+        haddr = addr;
+        hwdata = data;
+        hburst = burst;
+
+        @(posedge clk);
+        if(hsize == 2'b00) begin
+            
+        end
+        else if(hsize == 2'b01) begin
+            
+        end
+        else if(hsize == 2'b10) begin
+            
+        end
+        else begin
+
+        end
+    end
+    endtask
+
+
     ahb_subordinate #() DUT (.*);
 
     initial begin
         n_rst = 1;
 
         reset_dut;
+        
 
         $finish;
     end
